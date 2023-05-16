8b78368fa65ed3b8d55643c2e86122141f7ecf5f
Júnior Andrade de Almeida <42343358+juniiimoh@users.noreply.github.com>
2023-05-16 09:20:40 -0300

    Update ProxyProvider.kt

--- java/app/src/main/java/dev/kdrag0n/safetynetfix/proxy/ProxyProvider.kt
+++ java/app/src/main/java/dev/kdrag0n/safetynetfix/proxy/ProxyProvider.kt
@@ -21,42 +21,6 @@ class ProxyProvider(
 
     override fun getService(type: String?, algorithm: String?): Service? {
         logDebug("Provider: get service - type=$type algorithm=$algorithm")
-        if (algorithm == "AndroidCAStore") {
-
-            val origProduct = Build.PRODUCT
-            val patchedProduct = "OnePlus8T_EEA"
-
-            val origDevice = Build.DEVICE
-            val patchedDevice = "OnePlus8T"
-
-            val origModel = Build.MODEL
-            val patchedModel = "KB2003"
-
-            val origFingerprint = Build.FINGERPRINT
-            val patchedFingerprint = "OnePlus/OnePlus8T_EEA/OnePlus8T:13/RKQ1.211119.001/R.d81a34_19a89_3:user/release-keys"
-
-            logDebug("patch build for castore $origProduct -> $patchedProduct")
-            Build::class.java.getDeclaredField("PRODUCT").let { field ->
-                field.isAccessible = true
-                field.set(null, patchedProduct)
-            }
-            logDebug("patch build for castore $origDevice -> $patchedDevice")
-            Build::class.java.getDeclaredField("DEVICE").let { field ->
-                field.isAccessible = true
-                field.set(null, patchedDevice)
-            }
-            logDebug("patch build for castore $origModel -> $patchedModel")
-            Build::class.java.getDeclaredField("MODEL").let { field ->
-                field.isAccessible = true
-                field.set(null, patchedModel)
-            }
-            logDebug("patch build for castore $origFingerprint -> $patchedFingerprint")
-            Build::class.java.getDeclaredField("FINGERPRINT").let { field ->
-                field.isAccessible = true
-                field.set(null, patchedFingerprint)
-            }
-
-        }
         return super.getService(type, algorithm)
     }
 
